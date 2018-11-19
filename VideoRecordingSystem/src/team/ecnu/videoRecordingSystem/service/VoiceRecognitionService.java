cecf60fdf81a974cf2983fca4b197951e8d1f832
sungo <sungo@sun>
2018-11-19 19:46:33 +0800

    newservice

---
+++ VideoRecordingSystem/src/team/ecnu/videoRecordingSystem/service/VoiceRecognitionService.java
@@ -0,0 +1,5 @@
+package team.ecnu.videoRecordingSystem.service;
+
+public interface VoiceRecognitionService {
+
+}
---
+++ VideoRecordingSystem/src/team/ecnu/videoRecordingSystem/service/VoiceRecognitionServiceimpl.java
@@ -0,0 +1,5 @@
+package team.ecnu.videoRecordingSystem.service;
+
+public class VoiceRecognitionServiceimpl implements VoiceRecognitionService {
+
+}
